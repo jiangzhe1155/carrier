5a9751d41b6840fcf0a26f8c983ddcaaff6160b2
jiangzhe1155 <1024741733@qq.com>
2020-06-28 06:43:22 +0800

    feat 断点续传数据库形势

---
+++ src/main/java/org/cn/jiangzhe/admin/entity/TFieStorage.java
@@ -0,0 +1,43 @@
+package org.cn.jiangzhe.admin.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+import java.util.Date;
+
+/**
+ * (TFieStorage)
+ *
+ * @author jz
+ * @since 2020-06-26
+ */
+@Data
+@Accessors(chain = true)
+public class TFieStorage {
+
+    @TableId(type = IdType.AUTO)
+    private Long id;
+
+    @TableField("`path`")
+    private String path;
+
+    @TableField("`identifier`")
+    private String identifier;
+
+
+    @TableField("`md5`")
+    private String md5;
+
+    @TableField("`status`")
+    private FileStatusEnum status;
+
+    @TableField("`create_time`")
+    private Date createTime;
+
+    @TableField("`update_time`")
+    private Date updateTime;
+
+}
--- web-admin/src/components/FileUpload.vue
+++ web-admin/src/components/FileUpload.vue
@@ -3,7 +3,9 @@
     <uploader ref="uploader" :options="options" class="uploader-example" :file-status-text="statusText"
               :autoStart="true"
               @file-added="onFileAdded"
-              @file-success="onFileSuccess">
+              @file-success="onFileSuccess"
+              @file-progress="onFileProgress"
+    >
         <uploader-unsupport></uploader-unsupport>
         <uploader-drop>
             <uploader-btn>上传文件</uploader-btn>
@@ -16,6 +18,7 @@
 <script lang="ts">
     import {Component, Prop, Vue} from 'vue-property-decorator';
     import {Message} from "element-ui";
+    import SparkMD5 from 'spark-md5';
 
     @Component
     export default class FileUpload extends Vue {
@@ -27,7 +30,12 @@
             testChunks: false,
             maxChunkRetries: 3,
             allowDuplicateUploads: true,
-            simultaneousUploads: 1,
+            simultaneousUploads: 3,
+            checkChunkUploadedByResponse: function (chunk, message) {
+                let objMessage = JSON.parse(message);
+                console.log(chunk, message)
+                return true;
+            },
         };
         statusText = {
             success: '成功了',
@@ -41,19 +49,69 @@
             return this.$refs.uploader.uploader;
         }
 
+        onFileProgress(rootFile, file, chunk) {
+            console.log(`上传中 ${file.name}，chunk：${chunk.startByte / 1024 / 1024} ~ ${chunk.endByte / 1024 / 1024}`)
+        }
+
+
         onFileAdded(file) {
             file.relativePath = this.relativePath + "/" + file.relativePath;
+            this.computeMD5(file);
+
+
+        }
+
+        computeMD5(file) {
+            let fileReader = new FileReader();
+            let time = new Date().getTime();
+            let blobSlice = File.prototype.slice || File.prototype.mozSlice || File.prototype.webkitSlice;
+            let currentChunk = 0;
+            const chunkSize = 10 * 1024 * 1000;
+            let chunks = Math.ceil(file.size / chunkSize);
+            let spark = new SparkMD5.ArrayBuffer();
+
+            file.error = true
             file.pause();
-            this.http.post("preCreate", {
-                "identifier": file.uniqueIdentifier,
-                "totalSize": file.size
-            }).then(data => {
-                file.storageId = data.data.id;
-                file.resume()
-            })
+            loadNext();
+            fileReader.onload = (e => {
+                spark.append(e.target.result);
+                if (currentChunk < chunks) {
+                    currentChunk++;
+                    loadNext();
+                } else {
+                    let md5 = spark.end();
+                    file.md5 = md5;
+                    this.http.post("preCreate", {
+                        "identifier": file.uniqueIdentifier,
+                        "totalSize": file.size,
+                        "md5": file.md5
+                    }).then(data => {
+                        file.storageId = data.data.id;
+                    });
+                    console.log(`MD5计算完毕：${file.name} \nMD5：${md5} \n分片：${chunks} 大小:${file.size} 用时：${new Date().getTime() - time} ms`);
+                    // file.resume()
+                }
+            });
+            fileReader.onerror = function () {
+                this.error(`文件${file.name}读取出错，请检查该文件`);
+                file.cancel();
+            };
+
+            function loadNext() {
+                let start = currentChunk * chunkSize;
+                let end = ((start + chunkSize) >= file.size) ? file.size : start + chunkSize;
+                fileReader.readAsArrayBuffer(blobSlice.call(file.file, start, end));
+            }
         }
 
         onFileSuccess(rootFile, file, message, chunk) {
+
+            let res = JSON.parse(message);
+            if (res.code !== 0) {
+                Message.error("抱歉失败");
+                file.status = "error";
+            }
+
             this.http.post("merge", {
                 totalSize: file.size,
                 identifier: file.uniqueIdentifier,
