b0b62be2b3b673455c814731446512e416e6fbb4
jiangzhe1155 <1024741733@qq.com>
2020-07-08 07:15:10 +0800

    refactor 优化后端文件结构

--- web-admin/src/main.ts
+++ web-admin/src/main.ts
@@ -19,7 +19,7 @@ Vue.prototype.http = http;
 const originalPush = Router.prototype.push
 Router.prototype.push = function push(location) {
   return originalPush.call(this, location).catch(err => err)
-}
+};
 
 new Vue({
     router,
